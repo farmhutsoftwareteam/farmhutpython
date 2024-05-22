addc299e5236155003831c5d1529c5fdaee1ab49
Munyaradzi Makosa <munya@farmhutafrica.com>
2024-05-22 12:06:02 +0200

    fix : Update Cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_cors import CORS
 from .config import Config
-
 
 
 def create_app():
     # Configure logging as per Config
     
-    
     app = Flask(__name__)
     # Load configuration from `config.py`
     app.config.from_object(Config)
+    
+    # Enable CORS for all origins
+    CORS(app, resources={r"/*": {"origins": "*"}})
     
     # Initialize MongoDB
     mongo = PyMongo(app)
